917abe332cba022414df0d549c90a7c923d80c5f
PPDuck52 <ppduck1224@gmail.com>
2024-12-27 16:21:35 +0800

    2024.12.27

--- final.R
+++ final.R
@@ -1,14 +1,40 @@
 library(tidyverse)
 
-# Import the CSV file
-googleplaystore <- read.csv("googleplaystore.csv")
+raw_data <- read.csv("googleplaystore.csv")
+#view(raw_data)
+#CLEAN DATA
+cleaned_data <- raw_data |>
+  dplyr::filter(
+    !is.na(Rating),
+    !is.na(Type),
+    !is.na(`Content.Rating`),
+    !is.na(`Current.Ver`),
+    !is.na(`Android.Ver`)
+  )
 
-# Display the dataset
-glimpse(googleplaystore)
+cleaned_data <- cleaned_data[-c(9118), ]
+cleaned_data <- cleaned_data |> 
+  select(-Size, -Last.Updated, -Current.Ver, -Android.Ver, -Genres)
 
-# Count the occurrences of each category and sort them in descending order
-category_counts <- googleplaystore %>%
-  count(Category, sort = TRUE)
+#glimpse(cleaned_data)
 
-# Display the sorted categories
-#view(category_counts)
+#mutate variables
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+prepared_data <- cleaned_data |>
+  dplyr::mutate(
+    # Clean Installs column to numeric
+    installs_numeric = as.numeric(stringr::str_remove_all(Installs, "[+,]")),
+    
+    # Categorize apps as Free or Paid based on Price
+    price_category = ifelse(Price == "0", "Free", "Paid"),
+    
+    # Log-transform Reviews (add 1 to avoid log(0))
+    log_reviews = log(as.numeric(Reviews) + 1)
+    
+  )
+
+view(prepared_data)
+glimpse(prepared_data)
